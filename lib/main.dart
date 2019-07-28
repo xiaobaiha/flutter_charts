5f7ef0608e514757ee810ad1b516e1ae7f6f4460
feizhijun <feizhijun@bytedance.com>
2019-07-28 15:12:04 +0800

    feat: page route

--- lib/main.dart
+++ lib/main.dart
@@ -21,27 +21,67 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords>{
   final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(
+        alreadySaved? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
-  Widget _biggerSuggestions() {
+  Widget _buildSuggestions() {
     return new ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemBuilder: (context, i) {
         if(i.isOdd) return new Divider();
         final index = i ~/ 2;
-        if (index > _suggestions.length) {
+        if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
       },
+    );
+  }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) => new ListTile(
+              title: new Text(pair.asPascalCase, style: _biggerFont,),
+            )
+          );
+          final divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles
+            )
+            .toList();
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('new scaffold'),
+            ),
+            body: new ListView(children: divided,),
+          );
+        }
+      )
     );
   }
   @override
@@ -49,8 +89,11 @@ class RandomWordsState extends State<RandomWords>{
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('list view'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved,)
+        ],
       ),
-      body: _biggerSuggestions(),
+      body: _buildSuggestions(),
     );
   }
 }
