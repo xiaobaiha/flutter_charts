516cb9fc541d0d85f7c9828a408f49c6b2747c08
feizhijun <feizhijun@bytedance.com>
2019-08-06 13:30:21 +0800

    feat: line chart

--- lib/chart/line_chart.dart
+++ lib/chart/line_chart.dart
@@ -4,11 +4,25 @@ import 'package:charts_flutter/flutter.dart' as charts;
 class LineChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("line chart"),
-      ),
-      body: Center(child: Text('this is line chart'),),
-    );
+    final data = [
+      new DemoDataStructure(0, 5),
+      new DemoDataStructure(1, 25),
+      new DemoDataStructure(2, 100),
+      new DemoDataStructure(3, 75),
+    ];
+    final List<charts.Series<DemoDataStructure, num>> seriesList = [new charts.Series<DemoDataStructure, int>(
+      id: 'Sales',
+      colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+      domainFn: (DemoDataStructure sales, _) => sales.year,
+      measureFn: (DemoDataStructure sales, _) => sales.sales,
+      data: data,
+    )];
+    return new charts.LineChart(seriesList);
   }
+}
+
+class DemoDataStructure {
+  final int year;
+  final int sales;
+  DemoDataStructure(this.year, this.sales);
 }
